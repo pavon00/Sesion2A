90a5547e6153860a38d56724d5c288099ba8dd87
usuario <usuario@pc20-022>
2023-10-03 10:45:28 +0200

    fin

--- src/cmepps/Calculadora.java
+++ src/cmepps/Calculadora.java
@@ -1,20 +1,25 @@
 package cmepps;
 
 public class Calculadora {
-	public int suma(int a, int b) {
+	public static int suma(int a, int b) {
 		return a + b;
 	}
 
-	public int resta(int a, int b) {
+	public static int resta(int a, int b) {
 		return a - b;
 	}
 
-	public int multiplica(int a, int b) {
+	public static int multiplica(int a, int b) {
 		return a * b;
 	}
 
-	public int divide(int a, int b) {
-		int division = a / b;
+	public static int divide(int a, int b) {
+		int division = 0;
+		try {
+			division = a / b;
+		} catch (java.lang.ArithmeticException e) {
+			return -1;
+		}
 		if (division == 0) {
 			return -1;
 		}
